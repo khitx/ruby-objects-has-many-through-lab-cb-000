e15912ce56470750f2f793463790bd532699c99f
Kathy Hudson <khitx@icloud.com>
2018-02-01 17:30:02 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,7 +8,7 @@ class Patient
 
     def add_appointment(appointment)
       @appointments << appointment
-      Appointment.patient = self
+      appointment.patient = self
     end
 
     def doctors
