dfb993d4f39554edc25c95461de0d38f756afa65
Kathy Hudson <khitx@icloud.com>
2018-02-01 17:03:42 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,5 @@
 class Genre
-    attr_accessor :name, :songs
+    attr_accessor :name
 
     def initialize(name)
       @name = name
@@ -10,9 +10,9 @@ class Genre
       @songs << song
     end
 
-#    def songs
-#      @songs
-#    end
+    def songs
+      @songs
+    end
 
     def artists
       @songs.collect { |e| e.artist  }
